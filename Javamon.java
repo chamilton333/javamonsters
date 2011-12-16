a697b847e7d90ffe5dc35e7fc2637dd905e88ad5
chamilton333@hotmail.com <chamilton333@hotmail.com@28e778a0-db9a-9d11-9baf-136a64ae011d>
2011-12-16 02:09:30 +0000

    Josh's Javamon class with a few custom additions, largely to make it GUI friendly.

--- Javamon.java
+++ Javamon.java
@@ -1,3 +1,7 @@
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+
 /********************************************
  * 
  * This class is used for the sole purpose of creating javamon, and getting, or editing their attributes. 
@@ -13,7 +17,7 @@ public class Javamon {
 	
 	
 	
-	
+	private String actionResult="Game Started";
 	
 	
 	enum type {fire, earth, water, air}
@@ -28,6 +32,8 @@ public class Javamon {
 	private int evasivenessAtt = 10;	// likelihood of opponent missing
 	private boolean canHeal = false;
 	private int healAmount = 10;
+	private ImageIcon image=null;
+	private double percentHP=100;
 	
 	//---------------------------------------------------------------------------------------------------------------------------------
 	//IMPORTANT!!! - If any instance data is added, make sure to add it to the custom constructor (labeled below as Custom Constructor
@@ -50,6 +56,13 @@ public class Javamon {
 		hp = h;
 	}
 	
+	public Javamon(String nam, int maxHP, int h){ ///CHRIS CUSTOM CONSTRUCTOR
+		name=nam;
+		name = nam;
+		MAX_HP = maxHP;
+		hp = h;
+	}
+	
 	public Javamon(String nam, type typ){	//Creates javamon with name and type of choice
 		
 		nam = name;
@@ -63,7 +76,7 @@ public class Javamon {
 	 * Custom Constructor below
 	 ****************************************/
 	
-	public Javamon(String nam, type typ, int h, int a, int d, boolean heal, int healamoun, int acc, int evade){		//Creates custom javamon with all instance data specified
+	public Javamon(String nam, type typ, int h, int a, int d, boolean heal, int healamoun, int acc, int evade,String imageName){		//Creates custom javamon with all instance data specified
 		name = nam;
 		type = typ;
 		hp = h;
@@ -74,6 +87,7 @@ public class Javamon {
 		canHeal = heal;
 		accuracyAtt = acc;
 		evasivenessAtt = evade;
+		image=new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource(imageName)));
 	}
 	
 
@@ -114,7 +128,12 @@ public class Javamon {
 		return evasivenessAtt;
 	}
 	
-	
+	public ImageIcon getImage(){														//ADDED BY CHRIS
+		return image;
+	}
+	public String getActionResult(){
+		return actionResult;
+	}
 	
 	/***************************************************************************************
 	 * Setters!
@@ -151,6 +170,14 @@ public class Javamon {
 		evasivenessAtt = evade;
 	}
 	
+	public void setImage(String name){
+		
+	}
+	
+	public void setActionResult(String name){
+		actionResult=name;
+		
+	}
 	
 	
 	// MIGHT WANT TO MODIFY toString() TO INCLUDE ALL DATA, OR MAYBE MAKE TWO DIFFERENT toString METHODS, ONE TO BE BASIC AND ONE TO BE MORE COMPLEX.
@@ -159,9 +186,19 @@ public class Javamon {
 	 * Returns, in a String, information about a javamon.
 	 **************************************************************/
 	
+	public String healthString(){
+		return new String(hp+"/"+ MAX_HP);
+	}
+	
 	public String toString(){
 		return name+": "+ "  Hitpoints: "+ hp +"  AttackAtt: "+attackAtt+"  DefenseAtt: "+ defenseAtt+"  Type: "+type;
 		
 	}
+
+	public double getPercentHP() {
+		return 100*((double)hp)/(MAX_HP);
+	}
+
+
 	
 }
